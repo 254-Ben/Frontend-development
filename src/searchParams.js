6e41c079b284e7c0a6e64f7f44b81ad2c8f66e95
254-Ben <obenard300@gmail.com>
2020-03-02 18:17:01 +0300

    learning frontend

---
+++ src/searchParams.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import pet, { ANIMALS } from '@frontendmasters/pet'
+import dropDown from './dropdown'
+
+const SearchParams = () => {
+    // replace location
+    const [location, updateLocation] = useState("Seattle, WA")
+    const [breeds, updateBreeds] = useState([]);
+    const [animal, AnimalDropdown] = dropDown("Animal", "dog", ANIMALS);
+    const [breed, BreedDropdown, updateBreed] = dropDown("Breed","",breeds)
+
+    useEffect(() => {
+        updateBreeds([]);
+        updateBreed("");
+        pet.breeds(animal).then(({ breeds }) => {
+            const breedItems = breeds.map(({ name }) => name);
+            updateBreeds(breedItems)
+        }, console.error)
+    }, [animal, updateBreed, updateBreeds])
+
+    return (
+        <div className="search-params">
+            {name}
+            <form>
+                <label htmlFor="location">
+                    location
+                    <input id="location"
+                        value={location}
+                        placeholder="Location"
+                        onChange={(e) => updateLocation
+                        (e.target.value)}
+                    />
+                </label>
+                <AnimalDropdown />
+                <BreedDropdown />
+                <button>submit</button>
+            </form>
+        </div>
+    );
+};
+
+export default SearchParams;
